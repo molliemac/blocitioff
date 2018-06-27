9d9de02396d3a5722e31b232809dc033fba572d5
Mollie McIntyre <mollie.mcintyre9@gmail.com>
2018-06-27 01:18:56 -0700

    priority and sorting added

--- app/scripts/controllers/ListCtrl.js
+++ app/scripts/controllers/ListCtrl.js
@@ -1,37 +1,56 @@
 (function() {
-  function ListCtrl($scope, $uibModal, List, Todo, $log) {
+  function ListCtrl($scope, $filter, $uibModal, List, Todo, $log) {
     $scope.lists = List.all;
     $scope.myList = false;
     $scope.currentList = null;
     $scope.todos = null;
+    $scope.priority = {
+      availableOptions: [
+      {id: '1', setting: 'High'},
+      {id: '2', setting: 'Medium'},
+      {id: '3', setting: 'Low'}
+      ],
+      selectedOption: {id: '3', setting: 'Low'}
+    };
 
     $scope.toggle = function() {
       $scope.myList = !$scope.myList;
-    }
+    };
 
     $scope.createList = function(newList) {
       var newList = $scope.newList.trim();
       List.addList(newList);
       $scope.newList = '';
-    }
+    };
 
     $scope.setCurrentList = function (list) {
       $scope.currentList = list;
       $scope.todos = Todo.getByListId($scope.currentList.$id);
       console.log($scope.todos);
-    }
+    };
 
     $scope.addTodo = function(newTodo) {
       var newTodo = $scope.newTodo.trim();
+      var todoPriority = newTodo.priority;
       Todo.createTodo({
         listId: $scope.currentList.$id,
         createdAt: firebase.database.ServerValue.TIMESTAMP,
         description: $scope.newTodo,
         status: 'active',
-        done: false
+        done: false,
+        priority: $scope.priority.selectedOption.setting
       });
+
       $scope.newTodo = '';
       $scope.newTodo.done = false;
+    };
+
+    $scope.todoSort = function() {
+      $scope.todos = $filter('orderBy')($scope.todos, 'priority');
+    }
+
+    $scope.completedTodo = function() {
+      $scope.todo.done = true;
     }
 
     $scope.open = function() {
@@ -53,5 +72,5 @@
 
   angular
     .module('blocItOff')
-    .controller('ListCtrl', ['$scope', '$uibModal', 'List', 'Todo', '$log', ListCtrl]);
+    .controller('ListCtrl', ['$scope', '$filter', '$uibModal', 'List', 'Todo', '$log', ListCtrl]);
 })();
